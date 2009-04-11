964c22b93373dfbd7561e87f434fc059b8d9bd3a
zhang <zhangmdev@gmail.com>
2009-04-12 00:11:20 +0800

    add release bat

---
+++ module/release.bat
@@ -0,0 +1,15 @@
+copy .\ribgen\meshRibgen.dll D:\wamp\www\prt_release\ribgen
+copy .\ribgen\meshRibgen.slim D:\wamp\www\prt_release\ribgen
+copy .\procedural\meshCacheProcedural.dll D:\wamp\www\prt_release\procedural
+copy .\slim\ace.slim D:\wamp\www\prt_release\slim
+copy .\slim\displacement_z.slim D:\wamp\www\prt_release\slim
+copy .\slim\eye.slim D:\wamp\www\prt_release\slim
+copy .\slim\fur.slim D:\wamp\www\prt_release\slim
+copy .\slim\light_z.slim D:\wamp\www\prt_release\slim
+copy .\slim\ylset.slim D:\wamp\www\prt_release\slim
+copy .\slim\z_colors_prt.slim D:\wamp\www\prt_release\slim
+copy .\slim\z_lights_hdr.slim D:\wamp\www\prt_release\slim
+copy .\slim\include\zprt.dll D:\wamp\www\prt_release\slim\include
+copy TOR.ini D:\wamp\www\prt_release
+copy slim.ini D:\wamp\www\prt_release
+copy rat_install.bat D:\wamp\www\prt_release
