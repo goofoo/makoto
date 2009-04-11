326a7d5d6b7f99545133dca43366d81067240c14
zhang <zhangmdev@gmail.com>
2009-04-12 00:30:02 +0800

    photosynthesis init

---
+++ photosynthesis/main.cpp
@@ -0,0 +1,14 @@
+/*
+Syntax:
+*/			
+#include <iostream>
+using namespace std;
+
+int main (int argc, char * const argv[]) {
+    // insert code here...
+    cout << "Photosynthesis 0.0.0 04/12/09"<<endl;
+
+	return 0;
+
+}
+//:~
