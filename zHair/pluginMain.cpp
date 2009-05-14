81bd9b3dbd0ee1d582400a216e651aa43cf72dd6
zhang <zhangmdev@gmail.com>
2009-05-14 13:57:19 +0800

    minor

--- zHair/pluginMain.cpp
+++ zHair/pluginMain.cpp
@@ -21,7 +21,7 @@
 MStatus initializePlugin( MObject obj )
 { 
 	MStatus   status;
-	MFnPlugin plugin( obj, "Zhang", "0.1.8 - 05/13/09", "Any");
+	MFnPlugin plugin( obj, "Zhang", "0.2.1 - 05/14/09", "Any");
 
 	status = plugin.registerNode( "ZHairViz", HairNode::id, 
 						 &HairNode::creator, &HairNode::initialize,
