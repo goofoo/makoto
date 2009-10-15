85149573cfc02805ef25b0b1caa4da8dc3c9e12d
palladiun at night <pan@glycine.(none)>
2009-10-16 01:58:12 +0800

    add cloud shader

---
+++ module/shaders/dusty.sl
@@ -0,0 +1,25 @@
+surface
+dusty(color keysh0 = color(1,1,1);
+color keysh1 = color(1,1,1);
+color keysh2 = color(1,1,1);
+color keysh3 = color(1,1,1);
+color keysh4 = color(1,1,1);
+color keysh5 = color(1,1,1);
+color keysh6 = color(1,1,1);
+color keysh7 = color(1,1,1);
+color keysh8 = color(1,1,1);
+color backsh0 = color(1,1,1);
+color backsh1 = color(1,1,1);
+color backsh2 = color(1,1,1);
+color backsh3 = color(1,1,1);
+color backsh4 = color(1,1,1);
+color backsh5 = color(1,1,1);
+color backsh6 = color(1,1,1);
+color backsh7 = color(1,1,1);
+color backsh8 = color(1,1,1);)
+{
+	Oi = Os;
+	Ci = Cs;
+Ci = backsh4;
+	Ci *= Oi;
+}
--- pmap/RCloud.cpp
+++ pmap/RCloud.cpp
@@ -19,7 +19,7 @@ void RCloud::sphereRand(XYZ& q, int seed)
 	q.z = u;
 }
 
-RCloud::RCloud()
+RCloud::RCloud():coe0(0), coe1(0)
 {
 }
 
@@ -70,11 +70,85 @@ void RCloud::emit()
 	RtPointer vals[32];
 	vals[0] = (RtPointer)pp; vals[1] = (RtPointer)pw; vals[2] = (RtPointer)po;
 	
-	keys[3] = "constant color keysh0";
-	RtColor keysh0 = {coe0[0].x, coe0[0].y, coe0[0].z};
-	vals[3] = (RtPointer)&keysh0;
-	
-	RiPointsV(numDot,4,keys,vals);
+	int nparam = 3;
+	if(coe0) {
+		keys[nparam] = "constant color keysh0";
+		RtColor keysh0 = {coe0[0].x, coe0[0].y, coe0[0].z};
+		vals[nparam] = (RtPointer)&keysh0;
+		nparam++;
+		keys[nparam] = "constant color keysh1";
+		RtColor keysh1 = {coe0[1].x, coe0[1].y, coe0[1].z};
+		vals[nparam] = (RtPointer)&keysh1;
+		nparam++;
+		keys[nparam] = "constant color keysh2";
+		RtColor keysh2 = {coe0[2].x, coe0[2].y, coe0[2].z};
+		vals[nparam] = (RtPointer)&keysh2;
+		nparam++;
+		keys[nparam] = "constant color keysh3";
+		RtColor keysh3 = {coe0[3].x, coe0[3].y, coe0[3].z};
+		vals[nparam] = (RtPointer)&keysh3;
+		nparam++;
+		keys[nparam] = "constant color keysh4";
+		RtColor keysh4 = {coe0[4].x, coe0[4].y, coe0[4].z};
+		vals[nparam] = (RtPointer)&keysh4;
+		nparam++;
+		keys[nparam] = "constant color keysh5";
+		RtColor keysh5 = {coe0[5].x, coe0[5].y, coe0[5].z};
+		vals[nparam] = (RtPointer)&keysh5;
+		nparam++;
+		keys[nparam] = "constant color keysh1";
+		RtColor keysh6 = {coe0[6].x, coe0[6].y, coe0[6].z};
+		vals[nparam] = (RtPointer)&keysh6;
+		nparam++;
+		keys[nparam] = "constant color keysh1";
+		RtColor keysh7 = {coe0[7].x, coe0[7].y, coe0[7].z};
+		vals[nparam] = (RtPointer)&keysh7;
+		nparam++;
+		keys[nparam] = "constant color keysh1";
+		RtColor keysh8 = {coe0[8].x, coe0[8].y, coe0[8].z};
+		vals[nparam] = (RtPointer)&keysh8;
+		nparam++;
+	}
+	if(coe1) {
+		keys[nparam] = "constant color backsh0";
+		RtColor backsh0 = {coe1[0].x, coe1[0].y, coe1[0].z};
+		vals[nparam] = (RtPointer)&backsh0;
+		nparam++;
+		keys[nparam] = "constant color backsh1";
+		RtColor backsh1 = {coe1[1].x, coe1[1].y, coe1[1].z};
+		vals[nparam] = (RtPointer)&backsh1;
+		nparam++;
+		keys[nparam] = "constant color backsh2";
+		RtColor backsh2 = {coe1[2].x, coe1[2].y, coe1[2].z};
+		vals[nparam] = (RtPointer)&backsh2;
+		nparam++;
+		keys[nparam] = "constant color backsh3";
+		RtColor backsh3 = {coe1[3].x, coe1[3].y, coe1[3].z};
+		vals[nparam] = (RtPointer)&backsh3;
+		nparam++;
+		keys[nparam] = "constant color backsh4";
+		RtColor backsh4 = {coe1[4].x, coe1[4].y, coe1[4].z};
+		vals[nparam] = (RtPointer)&backsh4;
+		nparam++;
+		keys[nparam] = "constant color backsh5";
+		RtColor backsh5 = {coe1[5].x, coe1[5].y, coe1[5].z};
+		vals[nparam] = (RtPointer)&backsh5;
+		nparam++;
+		keys[nparam] = "constant color backsh1";
+		RtColor backsh6 = {coe1[6].x, coe1[6].y, coe1[6].z};
+		vals[nparam] = (RtPointer)&backsh6;
+		nparam++;
+		keys[nparam] = "constant color backsh1";
+		RtColor backsh7 = {coe1[7].x, coe1[7].y, coe1[7].z};
+		vals[nparam] = (RtPointer)&backsh7;
+		nparam++;
+		keys[nparam] = "constant color backsh1";
+		RtColor backsh8 = {coe1[8].x, coe1[8].y, coe1[8].z};
+		vals[nparam] = (RtPointer)&backsh8;
+		nparam++;
+	}
+
+	RiPointsV(numDot,nparam,keys,vals);
 	
 	delete[] pp;
 	delete[] pw;
